7dc470acdd462d145fad6f7120acd3bd2cf16d14
yegor <astery227@gmail.com>
2024-05-11 17:39:44 +0300

    fix: prevent user from selecting images not working

--- client/src/components/ui/Image.tsx
+++ client/src/components/ui/Image.tsx
@@ -2,6 +2,14 @@ import React from "react";
 
 interface ImageProps extends React.ImgHTMLAttributes<HTMLImageElement> {}
 
-export default function Image({ src, alt, ...rest }: ImageProps) {
-    return <img src={src} alt={alt} {...rest} onDragStart={(e) => e.preventDefault()} />;
+export default function Image({ src, alt, className, ...rest }: ImageProps) {
+    return (
+        <img
+            src={src}
+            alt={alt}
+            {...rest}
+            className={`select-none ${className} `}
+            onDragStart={(e) => e.preventDefault()}
+        />
+    );
 }

7564b9ce21d373f3d34d30dc85c0854cc809bb4b
yegor <astery227@gmail.com>
2024-05-11 17:18:41 +0300

    feat: remove the ability for user to drag an image with their mouse

--- client/src/pages/AddPhoneNumber/AddPhoneNumberPage.tsx
+++ client/src/pages/AddPhoneNumber/AddPhoneNumberPage.tsx
@@ -1,7 +1,7 @@
 import BaseLoginPage from "@/pages/BaseLogin/BaseLoginPage.tsx";
 import signupPrimaryImage from "@/pages/Signup/images/signup_image.jpg";
 import AddPhoneNumberForm from "@/pages/AddPhoneNumber/components/AddPhoneNumberForm.tsx";
-import "@/pages/AddPhoneNumber/styles/index.css";
+import "@/pages/AddPhoneNumber/add-phone-number.css";
 import api from "@/lib/api.ts";
 import { redirect } from "react-router-dom";
 
